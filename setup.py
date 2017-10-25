7a90f007c41c5a5a9c29c017ee1cb10c482c582f
redaxmedia <info@redaxmedia.com>
2017-10-26 01:14:48 +0200

    Use pandoc to convert markdown

--- setup.py
+++ setup.py
@@ -1,11 +1,13 @@
 #!/usr/bin/env python3
 
 from setuptools import setup
+from pypandoc import convert
 
 setup(
 	name = 'chroma-feedback',
 	description = 'Turn your Razer keyboard, mouse or headphone into a extreme feedback device for Travis CI',
-	version = '1.0.0',
+	long_description = convert('README.md', 'rst'),
+	version = '1.0.1',
 	license = 'GPL-3.0',
 	url = 'https://github.com/redaxmedia/chroma-feedback',
 	author = 'Henry Ruhs',
