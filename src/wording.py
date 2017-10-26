608097561d73d63d98d4803820f7d1621eab72e1
redaxmedia <info@redaxmedia.com>
2017-10-26 21:28:58 +0200

    Make Travis happy

--- src/wording.py
+++ src/wording.py
@@ -1,17 +1,17 @@
 wording =\
 {
-	'daemon_no': 'Daemon not found',
-	'build_process': 'Build of {} in process',
-	'build_failed': 'Build of {} failed',
-	'setting_passed': 'Setting {} to build passed',
-	'setting_process': 'Setting {} to build in process',
-	'setting_failed': 'Setting {} to build failed',
-	'goodbye': 'Goodbye',
-	'point': '.',
-	'comma': ',',
-	'colon': ':',
-	'question_mark': '?',
-	'exclamation_mark': '!'
+				'daemon_no': 'Daemon not found',
+				'build_process': 'Build of {} in process',
+				'build_failed': 'Build of {} failed',
+				'setting_passed': 'Setting {} to build passed',
+				'setting_process': 'Setting {} to build in process',
+				'setting_failed': 'Setting {} to build failed',
+				'goodbye': 'Goodbye',
+				'point': '.',
+				'comma': ',',
+				'colon': ':',
+				'question_mark': '?',
+				'exclamation_mark': '!'
 }
 
 
