89b0c8e46e6bc38e88482664257054f8564751d5
redaxmedia <info@redaxmedia.com>
2017-11-05 16:34:34 +0100

    Fix process status

--- src/core.py
+++ src/core.py
@@ -39,19 +39,20 @@ def run(slug, interval):
 	# process data
 
 	for item in data:
-		if item['last_build_state'] == 'unknown':
-			print(color.get('yellow') + wording.get('hourglass') + color.get('end') + ' ' + wording.get('build_process').format(item['slug']))
-			if status != 'errored' and status != 'failed':
-				status = 'process'
-		if item['last_build_state'] == 'errored':
-			print(color.get('white') + wording.get('cross') + color.get('end') + ' ' + wording.get('build_errored').format(item['slug']))
-			if status != 'failed':
-				status = 'errored'
-		if item['last_build_state'] == 'passed':
-			print(color.get('green') + wording.get('tick') + color.get('end') + ' ' + wording.get('build_passed').format(item['slug']))
-		if item['last_build_state'] == 'failed':
-			print(color.get('red') + wording.get('cross') + color.get('end') + ' ' + wording.get('build_failed').format(item['slug']))
-			status = 'failed'
+		if item['active'] is True:
+			if item['last_build_finished_at'] is None:
+				print(color.get('yellow') + wording.get('hourglass') + color.get('end') + ' ' + wording.get('build_process').format(item['slug']))
+				if status != 'errored' and status != 'failed':
+					status = 'process'
+			if item['last_build_state'] == 'errored':
+				print(color.get('white') + wording.get('cross') + color.get('end') + ' ' + wording.get('build_errored').format(item['slug']))
+				if status != 'failed':
+					status = 'errored'
+			if item['last_build_state'] == 'passed':
+				print(color.get('green') + wording.get('tick') + color.get('end') + ' ' + wording.get('build_passed').format(item['slug']))
+			if item['last_build_state'] == 'failed':
+				print(color.get('red') + wording.get('cross') + color.get('end') + ' ' + wording.get('build_failed').format(item['slug']))
+				status = 'failed'
 
 	# handle device
 
