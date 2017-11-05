ae273e46eea2b7a5d684e73be78a1ac2e3634c5e
redaxmedia <info@redaxmedia.com>
2017-11-05 16:40:26 +0100

    Switch to apiv2 for errored status (#10)
    * Switch to xml for errored status
    
    * Switch to xml for errored status
    
    * Switch to api v2 for errored status
    
    * Switch to api v2 for errored status
    
    * Finalize the readme.MD
    
    * Remove the response variable
    
    * Remove the response variable
    
    * Fix process status
    
    * Change the ordering

--- src/core.py
+++ src/core.py
@@ -1,5 +1,4 @@
 from __future__ import print_function
-import sys
 import os
 import threading
 import requests
@@ -9,47 +8,56 @@ import src.wording as wording
 try:
 	from openrazer.client import DeviceManager, DaemonNotFound
 except ImportError:
-	print(wording.get('driver_no') + wording.get('exclamation_mark'))
-	exit(1)
+	exit(wording.get('driver_no') + wording.get('exclamation_mark'))
 
 try:
 	device_manager = DeviceManager()
 	device_manager.sync_effects = True
 except DaemonNotFound:
-	print(wording.get('daemon_no') + wording.get('exclamation_mark'))
-	exit(1)
+	exit(wording.get('daemon_no') + wording.get('exclamation_mark'))
 
 
 def run(slug, interval):
-	data = requests.get('https://api.travis-ci.org/repos/' + slug).json()
+	data = requests.get('https://api.travis-ci.org/repos/' + slug, headers =
+	{
+		'accept': 'application/vnd.travis-ci.2+json'
+	}).json()
 	status = 'passed'
 
 	# handle data
 
+	if 'repos' in data:
+		data = data['repos']
+	if 'repo' in data:
+		data =\
+		[
+			data['repo']
+		]
 	if len(data) == 0:
-		sys.exit(1)
-	if isinstance(data, dict):
-		data = [data]
+		exit(wording.get('data_no') + wording.get('exclamation_mark'))
 
 	# process data
 
 	for item in data:
 		if item['active'] is True:
-			if item['last_build_status'] is None:
+			if item['last_build_finished_at'] is None:
 				print(color.get('yellow') + wording.get('hourglass') + color.get('end') + ' ' + wording.get('build_process').format(item['slug']))
-				if status != 'failed':
+				if status != 'errored' and status != 'failed':
 					status = 'process'
-			if item['last_build_status'] == 0:
+			if item['last_build_state'] == 'passed':
 				print(color.get('green') + wording.get('tick') + color.get('end') + ' ' + wording.get('build_passed').format(item['slug']))
-			if item['last_build_status'] == 1:
+			if item['last_build_state'] == 'errored':
+				print(color.get('white') + wording.get('cross') + color.get('end') + ' ' + wording.get('build_errored').format(item['slug']))
+				if status != 'failed':
+					status = 'errored'
+			if item['last_build_state'] == 'failed':
 				print(color.get('red') + wording.get('cross') + color.get('end') + ' ' + wording.get('build_failed').format(item['slug']))
 				status = 'failed'
 
 	# handle device
 
 	if len(device_manager.devices) == 0:
-		print(wording.get('device_no') + wording.get('exclamation_mark'))
-		exit(1)
+		exit(wording.get('device_no') + wording.get('exclamation_mark'))
 
 	# process device
 
@@ -58,13 +66,19 @@ def run(slug, interval):
 			print(wording.get('setting_process').format(device.name) + wording.get('point'))
 		if status == 'passed' and static(device, [0, 255, 0]):
 			print(wording.get('setting_passed').format(device.name) + wording.get('point'))
+		if status == 'errored' and pulsate(device, [255, 255, 255]):
+			print(wording.get('setting_errored').format(device.name) + wording.get('point'))
 		if status == 'failed' and pulsate(device, [255, 0, 0]):
 			print(wording.get('setting_failed').format(device.name) + wording.get('point'))
 
 	# handle thread
 
 	if interval > 0:
-		threading.Timer(interval, run, args=[slug, interval]).start()
+		threading.Timer(interval, run, args =
+		[
+			slug,
+			interval
+		]).start()
 
 
 def static(device, rgb):
