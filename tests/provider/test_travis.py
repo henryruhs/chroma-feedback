00b9e021ed8669234670c42023a1ba7a299bde80
redaxmedia <info@redaxmedia.com>
2018-02-21 23:09:17 +0100

    Rename testing methods

--- tests/provider/test_travis.py
+++ tests/provider/test_travis.py
@@ -1,11 +1,11 @@
 from src.provider import travis
 
 
-def test_travis_slug():
+def test_fetch_data_slug():
 	data = travis.fetch_data('redaxmedia/chroma-feedback')
 	assert len(data) == 1
 
 
-def test_travis_user():
+def test_fetch_data_user():
 	data = travis.fetch_data('redaxmedia')
 	assert len(data) > 1
