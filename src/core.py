a92422ca8322898b3dc5029625579409111ac220
redaxmedia <info@redaxmedia.com>
2018-02-23 22:13:49 +0100

    Introduce device

--- src/core.py
+++ src/core.py
@@ -1,6 +1,7 @@
 from __future__ import print_function
 import os
 import threading
+import src.device as device
 import src.miner as miner
 import src.reporter as reporter
 import src.wording as wording
@@ -40,7 +41,7 @@ def run(args):
 	# process device
 
 	if args.dry_run is False:
-		result = process_device(result['status'])
+		result = device.process_device(device_manager.devices, result['status'])
 		if result:
 			reporter.print_result(result)
 			print()
@@ -52,39 +53,6 @@ def run(args):
 		[
 			args
 		]).start()
-
-
-def process_device(status):
-	message = []
-
-	# process device
-
-	for device in device_manager.devices:
-		if status == 'passed' and static(device.fx, [0, 255, 0]):
-			message.append(wording.get('setting_passed').format(device.name) + wording.get('point'))
-		if status == 'process' and static(device.fx, [255, 255, 0]):
-			message.append(wording.get('setting_process').format(device.name) + wording.get('point'))
-		if status == 'errored' and pulsate(device.fx, [255, 255, 255]):
-			message.append(wording.get('setting_errored').format(device.name) + wording.get('point'))
-		if status == 'failed' and pulsate(device.fx, [255, 0, 0]):
-			message.append(wording.get('setting_failed').format(device.name) + wording.get('point'))
-	return\
-	{
-		'message': message,
-		'status': status
-	}
-
-
-def static(fx, rgb):
-	if fx.has('logo') and fx.has('scroll'):
-		return fx.misc.logo.static(rgb[0], rgb[1], rgb[2]) and fx.misc.scroll_wheel.static(rgb[0], rgb[1], rgb[2])
-	return fx.static(rgb[0], rgb[1], rgb[2])
-
-
-def pulsate(fx, rgb):
-	if fx.has('logo') and fx.has('scroll'):
-		return fx.misc.logo.pulsate(rgb[0], rgb[1], rgb[2]) and fx.misc.scroll_wheel.pulsate(rgb[0], rgb[1], rgb[2])
-	return fx.breath_single(rgb[0], rgb[1], rgb[2])
 
 
 def destroy(number, frame):
