41093c84b961071e27ed004b2e3aa6b9f91ccc3b
redaxmedia <info@redaxmedia.com>
2018-03-03 15:34:57 +0100

    Unit testing for miner (#21)
    * Unit testing for the miner
    
    * use unittest.mock over third party mock
    
    * Fix unit testing for miner
    
    * Fix unit testing for miner
    
    * Revert "use unittest.mock over third party mock"
    
    This reverts commit e914dc4
    
    * Revert "use unittest.mock over third party mock"
    
    This reverts commit e914dc4

---
+++ tests/test_miner.py
@@ -0,0 +1,21 @@
+from mock import MagicMock
+from src import miner
+
+
+def test_progress(mocker):
+	args = MagicMock()
+	args.host = None
+	args.provider = []
+	args.provider.append('appveyor')
+	args.provider.append('circle')
+	args.provider.append('gitlab')
+	args.provider.append('jenkins')
+	args.provider.append('teamcity')
+	args.provider.append('travis')
+	args.slug = []
+	args.slug.append('one')
+	args.slug.append('two')
+	args.token = None
+	fetch = mocker.spy(miner, 'fetch')
+	miner.process(args)
+	assert fetch.call_count == 12
