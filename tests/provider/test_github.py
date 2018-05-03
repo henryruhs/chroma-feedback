c6cef5b9e77f097624df2bfaca13dd2864014ce5
redaxmedia <info@redaxmedia.com>
2018-05-03 23:31:32 +0200

    Token for GitHub testing

--- tests/provider/test_github.py
+++ tests/provider/test_github.py
@@ -1,12 +1,17 @@
+import os
+import pytest
 from src.provider import github
 
 
 def test_fetch_slug():
-	data = github.fetch(None, 'redaxmedia/chroma-feedback', 'test')
-	assert data[0]['provider'] == 'github'
-	assert data[0]['slug'] == 'redaxmedia/chroma-feedback'
-	assert data[0]['active'] is True
-	assert data[0]['status']
+	if 'GITHUB_TOKEN' in os.environ:
+		data = github.fetch(None, 'redaxmedia/chroma-feedback', os.environ['GITHUB_TOKEN'])
+		assert data[0]['provider'] == 'github'
+		assert data[0]['slug'] == 'redaxmedia/chroma-feedback'
+		assert data[0]['active'] is True
+		assert data[0]['status']
+	else:
+		pytest.skip('GITHUB_TOKEN not defined')
 
 
 def test_fetch_invalid():
