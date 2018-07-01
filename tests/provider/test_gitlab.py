1efdc8db3e89457dbfbaedfd955996890c198d0a
redaxmedia <info@redaxmedia.com>
2018-07-01 12:48:15 +0200

    Switch to test dummy project on GitLab

--- tests/provider/test_gitlab.py
+++ tests/provider/test_gitlab.py
@@ -5,7 +5,7 @@ from src.provider import gitlab
 
 def test_fetch_user():
 	if 'GITLAB_TOKEN' in os.environ:
-		data = gitlab.fetch(None, '5583334', os.environ['GITLAB_TOKEN'])
+		data = gitlab.fetch(None, '7311836', os.environ['GITLAB_TOKEN'])
 		assert data[0]['provider'] == 'gitlab'
 		assert data[0]['active'] is True
 		assert data[0]['status']
