3fd7cdd24f2b2f8f3a0312b8de61e9630c31853a
Henry Ruhs <info@redaxmedia.com>
2018-07-01 12:56:00 +0200

    Gitlab latest jobs (#25)
    * Fetch latest GitLab jobs and switch to test dummy project on GitLab

--- src/provider/gitlab.py
+++ src/provider/gitlab.py
@@ -6,7 +6,25 @@ def fetch(host, slug, auth):
 	if host is None:
 		host = 'https://gitlab.com'
 	if slug and auth:
-		response = requests.get(host + '/api/v4/projects/' + slug + '/jobs', headers =
+		response = requests.get(host + '/api/v4/projects/' + slug + '/pipelines', headers =
+		{
+			'Private-Token': auth
+		})
+
+	# process response
+
+	if response and response.status_code == 200:
+		data = response.json()
+		pipeline = str(data[0]['id'])
+		if pipeline:
+			return fetch_jobs(host, slug, pipeline, auth)
+	return []
+
+
+def fetch_jobs(host, slug, pipeline, auth):
+	response = None
+	if host and slug and pipeline and auth:
+		response = requests.get(host + '/api/v4/projects/' + slug + '/pipelines/' + pipeline + '/jobs', headers =
 		{
 			'Private-Token': auth
 		})
@@ -17,6 +35,7 @@ def fetch(host, slug, auth):
 		data = response.json()
 		result = []
 		for project in data:
+			project['slug'] = slug
 			result.extend(normalize_data(project))
 		return result
 	return []
@@ -27,7 +46,7 @@ def normalize_data(project):
 	[
 		{
 			'provider': 'gitlab',
-			'slug': project['user']['username'] + '/' + project['name'],
+			'slug': project['user']['username'] + '/' + project['slug'] + '/' + project['name'],
 			'active': True,
 			'status': normalize_status(project['status'])
 		}
