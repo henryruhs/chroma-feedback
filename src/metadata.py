593328f8b25f5d2cb154cf3dfad9dfd3bee097cb
Henry Ruhs <info@redaxmedia.com>
2018-12-16 11:57:14 +0100

    Update metadata.py

--- src/metadata.py
+++ src/metadata.py
@@ -4,7 +4,7 @@ metadata =\
 	'description': 'Turn your Razer keyboard, mouse or headphone into a extreme feedback device',
 	'version': '3.6.0',
 	'license': 'GPL-3.0',
-	'keywords': 'razer chroma appveyor circle github gitlab jenkins teamcity travis ci',
+	'keywords': 'razer chroma appveyor circle github gitlab jenkins teamcity travis ci notification',
 	'author': 'Henry Ruhs',
 	'author_email': 'info@redaxmedia.com',
 	'url': 'https://github.com/redaxmedia/chroma-feedback'
