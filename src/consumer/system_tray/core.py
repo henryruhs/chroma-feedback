0ea38574126d49dc3c1c1f8df05e7fab6750416f
redaxmedia <info@redaxmedia.com>
2019-06-29 18:00:44 +0200

    Draw dynamic icon

--- src/consumer/system_tray/core.py
+++ src/consumer/system_tray/core.py
@@ -1,19 +1,25 @@
 import threading
-from PIL import Image
+from PIL import Image, ImageDraw
 from src import core, metadata, wording
 try:
 	from pystray import Icon, Menu, MenuItem
 except ImportError:
 	exit(wording.get('package_no').format('pystray') + wording.get('exclamation_mark'))
 
-
 def create_icon():
 	icon = Icon(metadata.get('name') + ' ' + metadata.get('version'))
 	icon.menu = Menu(
 		MenuItem(wording.get('stop'), core.destroy)
 	)
-	icon.icon = Image.open('src/consumer/system_tray/icon.png')
+	icon.icon = draw_image()
 	return icon
+
+
+def draw_image():
+	image = Image.new('RGBA', (100, 100), (0, 0, 0, 0))
+	draw = ImageDraw.Draw(image)
+	draw.ellipse((20, 20, 80, 80), fill = (255, 255, 255), outline = (0, 0, 0))
+	return image
 
 
 def run():
