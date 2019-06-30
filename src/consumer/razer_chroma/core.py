1d8919fd8c7e3e2b296d41fe9cc9b7a5ea329be0
redaxmedia <info@redaxmedia.com>
2019-06-30 23:32:01 +0200

    Fix unit testing for consumer

--- src/consumer/razer_chroma/core.py
+++ src/consumer/razer_chroma/core.py
@@ -1,17 +1,25 @@
 from src import wording
 
-try:
-	from openrazer.client import DeviceManager, DaemonNotFound
-except ImportError:
-	exit(wording.get('driver_no') + wording.get('exclamation_mark'))
-try:
-	device_manager = DeviceManager()
-	device_manager.sync_effects = True
-except DaemonNotFound:
-	exit(wording.get('daemon_no') + wording.get('exclamation_mark'))
-
 
 def run(status):
+
+	# handle driver
+
+	try:
+		from openrazer.client import DeviceManager, DaemonNotFound
+	except ImportError:
+		exit(wording.get('driver_no') + wording.get('exclamation_mark'))
+
+	# handle daemon
+
+	try:
+		device_manager = DeviceManager()
+		device_manager.sync_effects = True
+	except DaemonNotFound:
+		exit(wording.get('daemon_no') + wording.get('exclamation_mark'))
+
+	# process
+
 	if len(device_manager.devices) == 0:
 		exit(wording.get('device_no') + wording.get('exclamation_mark'))
 	return process(device_manager.devices, status)
