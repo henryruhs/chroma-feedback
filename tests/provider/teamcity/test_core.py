413d4404719dd36b055b7f0474262781b21ef73c
redaxmedia <info@redaxmedia.com>
2019-12-09 00:23:33 +0100

    Switch Teamcity from basic auth to bearer token

--- tests/provider/teamcity/test_core.py
+++ tests/provider/teamcity/test_core.py
@@ -2,6 +2,6 @@ from chroma_feedback.provider.teamcity.core import fetch
 
 
 def test_fetch_invalid() -> None:
-	result = fetch(None, None, None, None)
+	result = fetch(None, None, None)
 
 	assert result == []
