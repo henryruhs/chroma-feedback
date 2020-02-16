183dbb2078f643d2f65dfc608087f35a0ae3cb90
redaxmedia <info@redaxmedia.com>
2020-02-16 12:52:04 +0100

    Support for bamboo plan

--- tests/producer/bamboo/test_core.py
+++ tests/producer/bamboo/test_core.py
@@ -1,4 +1,30 @@
+import os
+import pytest
 from chroma_feedback.producer.bamboo.core import fetch
+
+
+def test_fetch_project_slug() -> None:
+	if 'BAMBOO_HOST' in os.environ and 'BAMBOO_PROJECT_SLUG' in os.environ and 'BAMBOO_USERNAME' in os.environ and 'BAMBOO_TOKEN' in os.environ:
+		result = fetch(os.environ['BAMBOO_HOST'], os.environ['BAMBOO_PROJECT_SLUG'], os.environ['BAMBOO_USERNAME'], os.environ['BAMBOO_TOKEN'])
+
+		assert result[0]['producer'] == 'bamboo'
+		assert result[0]['slug']
+		assert result[0]['active'] is True
+		assert result[0]['status']
+	else:
+		pytest.skip('BAMBOO_HOST and BAMBOO_PROJECT_SLUG and BAMBOO_USERNAME and BAMBOO_TOKEN must be defined.')
+
+
+def test_fetch_plan_slug() -> None:
+	if 'BAMBOO_HOST' in os.environ and 'BAMBOO_PLAN_SLUG' in os.environ and 'BAMBOO_USERNAME' in os.environ and 'BAMBOO_TOKEN' in os.environ:
+		result = fetch(os.environ['BAMBOO_HOST'], os.environ['BAMBOO_PLAN_SLUG'], os.environ['BAMBOO_USERNAME'], os.environ['BAMBOO_TOKEN'])
+
+		assert result[0]['producer'] == 'bamboo'
+		assert result[0]['slug']
+		assert result[0]['active'] is True
+		assert result[0]['status']
+	else:
+		pytest.skip('BAMBOO_HOST and BAMBOO_PLAN_SLUG and BAMBOO_USERNAME and BAMBOO_TOKEN must be defined.')
 
 
 def test_fetch_invalid() -> None:
