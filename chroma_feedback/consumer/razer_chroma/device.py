3170b2cd0ef79b637f3bf5e53cc6fc0526a2ba6b
jqassar <jqassar@users.noreply.github.com>
2020-03-07 05:45:27 -0800

    Fix chroma-feedback #51 by supporting multiple effects (#52)
    * Fix chroma-feedback #51 by supporting multiple effects
    
    * Address review comments and improve function logic.

--- chroma_feedback/consumer/razer_chroma/device.py
+++ chroma_feedback/consumer/razer_chroma/device.py
@@ -41,7 +41,7 @@ def process_devices(devices : Any, status : str) -> List[Dict[str, Any]]:
 				'consumer': 'razer_chroma',
 				'type': 'device',
 				'name': device.name,
-				'active': pulsate_device(device, color.get_errored()),
+				'active': pulsate_device(device, color.get_errored()) or breath_device(device, color.get_errored()),
 				'status': status
 			})
 		if status == 'failed':
@@ -50,7 +50,7 @@ def process_devices(devices : Any, status : str) -> List[Dict[str, Any]]:
 				'consumer': 'razer_chroma',
 				'type': 'device',
 				'name': device.name,
-				'active': pulsate_device(device, color.get_failed()),
+				'active': pulsate_device(device, color.get_failed()) or breath_device(device, color.get_failed()),
 				'status': status
 			})
 	return result
@@ -59,14 +59,27 @@ def process_devices(devices : Any, status : str) -> List[Dict[str, Any]]:
 def static_device(device : Any, state : Dict[str, Any]) -> bool:
 	if device.has('brightness'):
 		device.brightness = state['brightness'][0]
-	if device.fx.has('logo') and device.fx.has('scroll'):
-		return device.fx.misc.logo.static(state['rgb'][0], state['rgb'][1], state['rgb'][2]) and device.fx.misc.scroll_wheel.static(state['rgb'][0], state['rgb'][1], state['rgb'][2])
-	return device.fx.static(state['rgb'][0], state['rgb'][1], state['rgb'][2])
+	if device.fx.has('logo_static') or device.fx.has('scroll_static'):
+		return device.fx.misc.logo.static(state['rgb'][0], state['rgb'][1], state['rgb'][2]) | device.fx.misc.scroll_wheel.static(state['rgb'][0], state['rgb'][1], state['rgb'][2])
+	if device.fx.has('static'):
+		return device.fx.static(state['rgb'][0], state['rgb'][1], state['rgb'][2])
+	return False
 
 
 def pulsate_device(device : Any, state : Dict[str, Any]) -> bool:
 	if device.has('brightness'):
 		device.brightness = state['brightness'][0]
-	if device.fx.has('logo') and device.fx.has('scroll'):
-		return device.fx.misc.logo.pulsate(state['rgb'][0], state['rgb'][1], state['rgb'][2]) and device.fx.misc.scroll_wheel.pulsate(state['rgb'][0], state['rgb'][1], state['rgb'][2])
-	return device.fx.breath_single(state['rgb'][0], state['rgb'][1], state['rgb'][2])
+	if device.fx.has('logo_pulsate') or device.fx.has('scroll_pulsate'):
+		return device.fx.misc.logo.pulsate(state['rgb'][0], state['rgb'][1], state['rgb'][2]) | device.fx.misc.scroll_wheel.pulsate(state['rgb'][0], state['rgb'][1], state['rgb'][2])
+	if device.fx.has('pulsate'):
+		return device.fx.pulsate(state['rgb'][0], state['rgb'][1], state['rgb'][2])
+	return False
+
+def breath_device(device : Any, state : Dict[str, Any]) -> bool:
+	if device.has('brightness'):
+		device.brightness = state['brightness'][0]
+	if device.fx.has('logo_breath_single') or device.fx.has('scroll_breath_single'):
+		return device.fx.misc.logo.breath_single(state['rgb'][0], state['rgb'][1], state['rgb'][2]) | device.fx.misc.scroll_wheel.breath_single(state['rgb'][0], state['rgb'][1], state['rgb'][2])
+	if device.fx.has('breath_single'):
+		return device.fx.breath_single(state['rgb'][0], state['rgb'][1], state['rgb'][2])
+	return False
