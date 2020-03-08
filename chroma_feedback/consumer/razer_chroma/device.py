7b0587f0203e8377aefef641ec207cc87d7e80ed
redaxmedia <info@redaxmedia.com>
2020-03-08 14:48:08 +0100

    Revert to original misc lightning

--- chroma_feedback/consumer/razer_chroma/device.py
+++ chroma_feedback/consumer/razer_chroma/device.py
@@ -59,8 +59,8 @@ def process_devices(devices : Any, status : str) -> List[Dict[str, Any]]:
 def static_device(device : Any, state : Dict[str, Any]) -> bool:
 	if device.has('brightness'):
 		device.brightness = state['brightness'][0]
-	if device.fx.has('logo_static') and device.fx.has('scroll_static'):
-		return device.fx.misc.logo.static(state['rgb'][0], state['rgb'][1], state['rgb'][2]) and device.fx.misc.scroll_wheel.static(state['rgb'][0], state['rgb'][1], state['rgb'][2])
+	if device.fx.has('logo_static') or device.fx.has('scroll_static'):
+		return device.fx.misc.logo.static(state['rgb'][0], state['rgb'][1], state['rgb'][2]) | device.fx.misc.scroll_wheel.static(state['rgb'][0], state['rgb'][1], state['rgb'][2])
 	if device.fx.has('static'):
 		return device.fx.static(state['rgb'][0], state['rgb'][1], state['rgb'][2])
 	return False
@@ -69,8 +69,8 @@ def static_device(device : Any, state : Dict[str, Any]) -> bool:
 def pulsate_device(device : Any, state : Dict[str, Any]) -> bool:
 	if device.has('brightness'):
 		device.brightness = state['brightness'][0]
-	if device.fx.has('logo_pulsate') and device.fx.has('scroll_pulsate'):
-		return device.fx.misc.logo.pulsate(state['rgb'][0], state['rgb'][1], state['rgb'][2]) and device.fx.misc.scroll_wheel.pulsate(state['rgb'][0], state['rgb'][1], state['rgb'][2])
+	if device.fx.has('logo_pulsate') or device.fx.has('scroll_pulsate'):
+		return device.fx.misc.logo.pulsate(state['rgb'][0], state['rgb'][1], state['rgb'][2]) | device.fx.misc.scroll_wheel.pulsate(state['rgb'][0], state['rgb'][1], state['rgb'][2])
 	if device.fx.has('pulsate'):
 		return device.fx.pulsate(state['rgb'][0], state['rgb'][1], state['rgb'][2])
 	return False
@@ -79,8 +79,8 @@ def pulsate_device(device : Any, state : Dict[str, Any]) -> bool:
 def breath_device(device : Any, state : Dict[str, Any]) -> bool:
 	if device.has('brightness'):
 		device.brightness = state['brightness'][0]
-	if device.fx.has('logo_breath_single') and device.fx.has('scroll_breath_single'):
-		return device.fx.misc.logo.breath_single(state['rgb'][0], state['rgb'][1], state['rgb'][2]) and device.fx.misc.scroll_wheel.breath_single(state['rgb'][0], state['rgb'][1], state['rgb'][2])
+	if device.fx.has('logo_breath_single') or device.fx.has('scroll_breath_single'):
+		return device.fx.misc.logo.breath_single(state['rgb'][0], state['rgb'][1], state['rgb'][2]) | device.fx.misc.scroll_wheel.breath_single(state['rgb'][0], state['rgb'][1], state['rgb'][2])
 	if device.fx.has('breath_single'):
 		return device.fx.breath_single(state['rgb'][0], state['rgb'][1], state['rgb'][2])
 	return False
