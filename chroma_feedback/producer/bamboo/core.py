8ba1e6ea48f1965b6439ff3356e05b4994a197e7
Henry Ruhs <info@redaxmedia.com>
2020-03-31 04:15:01 +0200

    Bamboo revision (#67)
    * Revision on bamboo producer
    
    * Revision on bamboo producer part2
    
    * Revision on bamboo producer part2

--- chroma_feedback/producer/bamboo/core.py
+++ chroma_feedback/producer/bamboo/core.py
@@ -1,6 +1,5 @@
 from typing import Any, Dict, List
 from argparse import ArgumentParser
-import base64
 import requests
 from chroma_feedback import helper
 from .normalize import normalize_data
@@ -14,8 +13,7 @@ def init(program : ArgumentParser) -> None:
 	if not ARGS:
 		program.add_argument('--bamboo-host', required = True)
 		program.add_argument('--bamboo-slug', action = 'append', required = True)
-		program.add_argument('--bamboo-username', required = True)
-		program.add_argument('--bamboo-password', required = True)
+		program.add_argument('--bamboo-token', required = True)
 	ARGS = helper.get_first(program.parse_known_args())
 
 
@@ -23,20 +21,19 @@ def run() -> List[Dict[str, Any]]:
 	result = []
 
 	for slug in ARGS.bamboo_slug:
-		result.extend(fetch(ARGS.bamboo_host, slug, ARGS.bamboo_username, ARGS.bamboo_password))
+		result.extend(fetch(ARGS.bamboo_host, slug, ARGS.bamboo_token))
 	return result
 
 
-def fetch(host : str, slug : str, username : str, password : str) -> List[Dict[str, Any]]:
+def fetch(host : str, slug : str, token : str) -> List[Dict[str, Any]]:
 	result = []
 	response = None
 
-	if host and slug and username and password:
-		username_password = username + ':' + password
+	if host and slug and token:
 		response = requests.get(host + '/rest/api/latest/result/' + get_slug(slug), headers =
 		{
 			'Accept': 'application/json',
-			'Authorization': 'Basic ' + base64.b64encode(username_password.encode('utf-8')).decode('ascii')
+			'Authorization': 'Bearer ' + token
 		})
 
 	# process response
