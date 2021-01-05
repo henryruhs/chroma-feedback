f74862aa651b01a9cdd2f8349374380710948666
redaxmedia <info@redaxmedia.com>
2021-01-05 23:43:27 +0100

    Remove try / catch from consumer testing

--- tests/consumer/philips_hue/test_light.py
+++ tests/consumer/philips_hue/test_light.py
@@ -1,4 +1,3 @@
-import pytest
 try:
 	from unittest.mock import MagicMock
 except ImportError:
@@ -9,60 +8,48 @@ MOCK = MagicMock()
 
 
 def test_process_passed() -> None:
-	try:
-		result = process_lights(
-		{
-			MOCK
-		}, 'passed')
+	result = process_lights(
+	{
+		MOCK
+	}, 'passed')
 
-		assert result[0]['consumer'] == 'philips_hue'
-		assert result[0]['type'] == 'light'
-		assert result[0]['name']
-		assert result[0]['status'] == 'passed'
-	except:
-		pytest.skip()
+	assert result[0]['consumer'] == 'philips_hue'
+	assert result[0]['type'] == 'light'
+	assert result[0]['name']
+	assert result[0]['status'] == 'passed'
 
 
 def test_process_started() -> None:
-	try:
-		result = process_lights(
-		{
-			MOCK
-		}, 'started')
+	result = process_lights(
+	{
+		MOCK
+	}, 'started')
 
-		assert result[0]['consumer'] == 'philips_hue'
-		assert result[0]['type'] == 'light'
-		assert result[0]['name']
-		assert result[0]['status'] == 'started'
-	except:
-		pytest.skip()
+	assert result[0]['consumer'] == 'philips_hue'
+	assert result[0]['type'] == 'light'
+	assert result[0]['name']
+	assert result[0]['status'] == 'started'
 
 
 def test_process_errored() -> None:
-	try:
-		result = process_lights(
-		{
-			MOCK
-		}, 'errored')
+	result = process_lights(
+	{
+		MOCK
+	}, 'errored')
 
-		assert result[0]['consumer'] == 'philips_hue'
-		assert result[0]['type'] == 'light'
-		assert result[0]['name']
-		assert result[0]['status'] == 'errored'
-	except:
-		pytest.skip()
+	assert result[0]['consumer'] == 'philips_hue'
+	assert result[0]['type'] == 'light'
+	assert result[0]['name']
+	assert result[0]['status'] == 'errored'
 
 
 def test_process_failed() -> None:
-	try:
-		result = process_lights(
-		{
-			MOCK
-		}, 'failed')
+	result = process_lights(
+	{
+		MOCK
+	}, 'failed')
 
-		assert result[0]['consumer'] == 'philips_hue'
-		assert result[0]['type'] == 'light'
-		assert result[0]['name']
-		assert result[0]['status'] == 'failed'
-	except:
-		pytest.skip()
+	assert result[0]['consumer'] == 'philips_hue'
+	assert result[0]['type'] == 'light'
+	assert result[0]['name']
+	assert result[0]['status'] == 'failed'
--- tests/consumer/razer_chroma/test_device.py
+++ tests/consumer/razer_chroma/test_device.py
@@ -1,4 +1,3 @@
-import pytest
 try:
 	from unittest.mock import MagicMock
 except ImportError:
@@ -9,60 +8,48 @@ MOCK = MagicMock()
 
 
 def test_process_passed() -> None:
-	try:
-		result = process_devices(
-		{
-			MOCK
-		}, 'passed')
+	result = process_devices(
+	{
+		MOCK
+	}, 'passed')
 
-		assert result[0]['consumer'] == 'razer_chroma'
-		assert result[0]['type'] == 'device'
-		assert result[0]['name']
-		assert result[0]['status'] == 'passed'
-	except:
-		pytest.skip()
+	assert result[0]['consumer'] == 'razer_chroma'
+	assert result[0]['type'] == 'device'
+	assert result[0]['name']
+	assert result[0]['status'] == 'passed'
 
 
 def test_process_started() -> None:
-	try:
-		result = process_devices(
-		{
-			MOCK
-		}, 'started')
+	result = process_devices(
+	{
+		MOCK
+	}, 'started')
 
-		assert result[0]['consumer'] == 'razer_chroma'
-		assert result[0]['type'] == 'device'
-		assert result[0]['name']
-		assert result[0]['status'] == 'started'
-	except:
-		pytest.skip()
+	assert result[0]['consumer'] == 'razer_chroma'
+	assert result[0]['type'] == 'device'
+	assert result[0]['name']
+	assert result[0]['status'] == 'started'
 
 
 def test_process_errored() -> None:
-	try:
-		result = process_devices(
-		{
-			MOCK
-		}, 'errored')
+	result = process_devices(
+	{
+		MOCK
+	}, 'errored')
 
-		assert result[0]['consumer'] == 'razer_chroma'
-		assert result[0]['type'] == 'device'
-		assert result[0]['name']
-		assert result[0]['status'] == 'errored'
-	except SystemExit:
-		pytest.skip()
+	assert result[0]['consumer'] == 'razer_chroma'
+	assert result[0]['type'] == 'device'
+	assert result[0]['name']
+	assert result[0]['status'] == 'errored'
 
 
 def test_process_failed() -> None:
-	try:
-		result = process_devices(
-		{
-			MOCK
-		}, 'failed')
+	result = process_devices(
+	{
+		MOCK
+	}, 'failed')
 
-		assert result[0]['consumer'] == 'razer_chroma'
-		assert result[0]['type'] == 'device'
-		assert result[0]['name']
-		assert result[0]['status'] == 'failed'
-	except:
-		pytest.skip()
+	assert result[0]['consumer'] == 'razer_chroma'
+	assert result[0]['type'] == 'device'
+	assert result[0]['name']
+	assert result[0]['status'] == 'failed'
