877d0dd659e1c581bccd89d091648e57259b38f8
redaxmedia <info@redaxmedia.com>
2021-03-17 23:21:02 +0100

    Bump version and automate keywords

--- chroma_feedback/metadata.py
+++ chroma_feedback/metadata.py
@@ -1,10 +1,12 @@
+from chroma_feedback.producer import __all__ as producer
+
 METADATA =\
 {
 	'name': 'chroma-feedback',
 	'description': 'Turn your RGB powered hardware into an build indicator for continuous integration',
-	'version': '8.4.0',
+	'version': '8.5.0',
 	'license': 'MIT',
-	'keywords': 'appveyor bamboo circle codeship github gitlab jenkins teamcity travis',
+	'keywords': ' '.join(producer),
 	'author': 'Henry Ruhs',
 	'author_email': 'info@redaxmedia.com',
 	'url': 'https://github.com/redaxmedia/chroma-feedback'
