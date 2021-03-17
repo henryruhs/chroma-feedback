2b9d33ebfea412f1e4ce9c920f361043d0b5c808
redaxmedia <info@redaxmedia.com>
2021-03-17 23:05:06 +0100

    Enforce failed build

--- tests/producer/buddy/test_core.py
+++ tests/producer/buddy/test_core.py
@@ -4,10 +4,11 @@ from chroma_feedback.producer.buddy.core import fetch
 
 
 def test_fetch_slug() -> None:
+	inValid = True
 	if os.environ.get('BUDDY_TOKEN'):
 		result = fetch('https://api.buddy.works', 'redaxmedia/chroma-feedback', os.environ.get('BUDDY_TOKEN'))
 
-		assert result[0]['producer'] == 'buddy_'
+		assert result[0]['producer'] == 'buddy'
 		assert result[0]['slug'] == 'redaxmedia/chroma-feedback'
 		assert result[0]['active'] is True
 		assert result[0]['status']
