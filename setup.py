b7fcffc6c0e4e88686050aa0649c45e0cdd2fa12
redaxmedia <info@redaxmedia.com>
2021-11-08 19:50:12 +0100

    Update dependencies

--- setup.py
+++ setup.py
@@ -60,14 +60,14 @@ setup(
 	install_requires =
 	[
 		'asyncio==3.4.3',
-		'busylight-for-humans==0.12.10',
+		'busylight-for-humans==0.13.1',
 		'lifxlan==1.2.7',
 		'nanoleafapi==2.1.1',
 		'phue==1.1',
 		'python-magichue==0.2.9.3',
-		'pyqt5==5.15.4',
-		'pywizlight==0.4.7',
+		'pyqt5==5.15.6',
+		'pywizlight==0.4.12',
 		'requests==2.26.0',
-		'yeelight==0.7.4'
+		'yeelight==0.7.8'
 	]
 )
