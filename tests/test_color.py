f1ab1915e890440553d5eb7a602e42a4449c4e42
redaxmedia <info@redaxmedia.com>
2022-01-30 15:01:39 +0100

    Add more testing

--- tests/test_color.py
+++ tests/test_color.py
@@ -1,6 +1,14 @@
 from chroma_feedback import color
 
 
+def test_format_by_status() -> None:
+	assert color.format_by_status('__test__', 'passed') == '\033[0;32m__test__\033[0m'
+	assert color.format_by_status('__test__', 'started') == '\033[0;34m__test__\033[0m'
+	assert color.format_by_status('__test__', 'errored') == '\033[0;37m__test__\033[0m'
+	assert color.format_by_status('__test__', 'warned') == '\033[0;33m__test__\033[0m'
+	assert color.format_by_status('__test__', 'failed') == '\033[0;31m__test__\033[0m'
+
+
 def test_format_passed() -> None:
 	assert color.format_passed('__test__') == '\033[0;32m__test__\033[0m'
 
@@ -9,12 +17,24 @@ def test_format_started() -> None:
 	assert color.format_started('__test__') == '\033[0;34m__test__\033[0m'
 
 
+def test_format_errored() -> None:
+	assert color.format_errored('__test__') == '\033[0;37m__test__\033[0m'
+
+
 def test_format_warned() -> None:
 	assert color.format_warned('__test__') == '\033[0;33m__test__\033[0m'
 
 
 def test_format_failed() -> None:
 	assert color.format_failed('__test__') == '\033[0;31m__test__\033[0m'
+
+
+def test_get_by_status() -> None:
+	assert color.get_by_status('passed') == color.get_passed()
+	assert color.get_by_status('started') == color.get_started()
+	assert color.get_by_status('errored') == color.get_errored()
+	assert color.get_by_status('warned') == color.get_warned()
+	assert color.get_by_status('failed') == color.get_failed()
 
 
 def test_get_passed() -> None:
