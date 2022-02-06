520ce639688a3fe417ef0e1469926a436be665d9
redaxmedia <info@redaxmedia.com>
2022-02-06 22:21:59 +0100

    Try to fix appveyor build part4

--- tests/test_color.py
+++ tests/test_color.py
@@ -2,11 +2,18 @@ from chroma_feedback import color, helper
 
 
 def test_format_by_status() -> None:
-	assert color.format_by_status('__test__', 'passed') == '\033[0;32m__test__\033[0m'
-	assert color.format_by_status('__test__', 'started') == '\033[0;34m__test__\033[0m'
-	assert color.format_by_status('__test__', 'errored') == '\033[0;37m__test__\033[0m'
-	assert color.format_by_status('__test__', 'warned') == '\033[0;33m__test__\033[0m'
-	assert color.format_by_status('__test__', 'failed') == '\033[0;31m__test__\033[0m'
+	if helper.is_windows() is False:
+		assert color.format_by_status('__test__', 'passed') == '\033[0;32m__test__\033[0m'
+		assert color.format_by_status('__test__', 'started') == '\033[0;34m__test__\033[0m'
+		assert color.format_by_status('__test__', 'errored') == '\033[0;37m__test__\033[0m'
+		assert color.format_by_status('__test__', 'warned') == '\033[0;33m__test__\033[0m'
+		assert color.format_by_status('__test__', 'failed') == '\033[0;31m__test__\033[0m'
+	else:
+		assert color.format_by_status('__test__', 'passed') == '__test__'
+		assert color.format_by_status('__test__', 'started') == '__test__'
+		assert color.format_by_status('__test__', 'errored') == '__test__'
+		assert color.format_by_status('__test__', 'warned') == '__test__'
+		assert color.format_by_status('__test__', 'failed') == '__test__'
 
 
 def test_format_passed() -> None:
