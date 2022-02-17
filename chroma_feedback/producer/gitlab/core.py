26154e673f5fb8fb1ae2ad397aefc6804d6cd17c
redaxmedia <info@redaxmedia.com>
2022-02-17 23:50:21 +0100

    Gitlab without slug to fetch owned projects

--- chroma_feedback/producer/gitlab/core.py
+++ chroma_feedback/producer/gitlab/core.py
@@ -13,7 +13,7 @@ def init(program : ArgumentParser) -> None:
 
 	if not ARGS:
 		program.add_argument('--gitlab-host', default = 'https://gitlab.com')
-		program.add_argument('--gitlab-slug', action = 'append', required = True)
+		program.add_argument('--gitlab-slug', action = 'append')
 		program.add_argument('--gitlab-token', required = True)
 	ARGS = helper.get_first(program.parse_known_args())
 
@@ -21,12 +21,22 @@ def init(program : ArgumentParser) -> None:
 def run() -> List[Producer]:
 	result = []
 
-	for slug in ARGS.gitlab_slug:
-		pipeline_ids = fetch_pipeline_ids(ARGS.gitlab_host, slug, ARGS.gitlab_token)
+	if ARGS.gitlab_slug:
+		for slug in ARGS.gitlab_slug:
+			pipeline_ids = fetch_pipeline_ids(ARGS.gitlab_host, slug, ARGS.gitlab_token)
 
-		if pipeline_ids:
-			for pipeline_id in pipeline_ids:
-				result.extend(fetch(ARGS.gitlab_host, slug, pipeline_id, ARGS.gitlab_token))
+			if pipeline_ids:
+				for pipeline_id in pipeline_ids:
+					result.extend(fetch(ARGS.gitlab_host, slug, pipeline_id, ARGS.gitlab_token))
+	else:
+		project_ids = fetch_project_ids(ARGS.gitlab_host, ARGS.gitlab_token)
+
+		for project_id in project_ids:
+			pipeline_ids = fetch_pipeline_ids(ARGS.gitlab_host, project_id, ARGS.gitlab_token)
+
+			if pipeline_ids:
+			 	for pipeline_id in pipeline_ids:
+			 		result.extend(fetch(ARGS.gitlab_host, project_id, pipeline_id, ARGS.gitlab_token))
 	return result
 
 
@@ -45,6 +55,24 @@ def fetch(host : str, slug : str, pipeline_id : str, token : str) -> List[Produc
 		for build in data:
 			if 'name' in build and 'web_url' in build and 'status' in build:
 				result.append(normalize_data(slug + '/' + build['name'], build['web_url'], build['status']))
+	return result
+
+
+def fetch_project_ids(host : str, token : str) -> List[str]:
+	result = []
+	response = None
+
+	if host and  token:
+		response = request.get(host + '/api/v4/projects?owned=true', headers = create_headers(token))
+
+	# process response
+
+	if response and response.status_code == 200:
+		data = request.parse_json(response)
+
+		for project in data:
+			if 'id' in project:
+				result.append(str(project['id']))
 	return result
 
 
--- tests/producer/gitlab/test_core.py
+++ tests/producer/gitlab/test_core.py
@@ -20,8 +20,35 @@ def test_run_one() -> None:
 		result = gitlab.core.run()
 
 		assert result[0]['name'] == 'gitlab'
-		assert result[0]['slug']
+		assert result[0]['slug'] == '33658238/test'
 		assert 'https://gitlab.com/redaxmedia/chroma-feedback-test' in result[0]['url']
 		assert result[0]['status'] in get_args(Status)
+
+		assert result[1]['name'] == 'gitlab'
+		assert result[1]['slug'] == '33658238/lint'
+		assert 'https://gitlab.com/redaxmedia/chroma-feedback-test' in result[1]['url']
+		assert result[1]['status'] in get_args(Status)
+	else:
+		pytest.skip('GITLAB_TOKEN is not defined')
+
+
+def test_run_many() -> None:
+	if os.environ.get('GITLAB_TOKEN'):
+		gitlab.core.ARGS = argparse.Namespace(
+			gitlab_host = 'https://gitlab.com',
+			gitlab_slug = None,
+			gitlab_token = os.environ.get('GITLAB_TOKEN')
+		)
+		result = gitlab.core.run()
+
+		assert result[0]['name'] == 'gitlab'
+		assert result[0]['slug'] == '33658238/test'
+		assert 'https://gitlab.com/redaxmedia/chroma-feedback-test' in result[0]['url']
+		assert result[0]['status'] in get_args(Status)
+
+		assert result[1]['name'] == 'gitlab'
+		assert result[1]['slug'] == '33658238/lint'
+		assert 'https://gitlab.com/redaxmedia/chroma-feedback-test' in result[1]['url']
+		assert result[1]['status'] in get_args(Status)
 	else:
 		pytest.skip('GITLAB_TOKEN is not defined')
