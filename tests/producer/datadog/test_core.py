7cef5f904ec8c3e067bad9be57cabf12bd99cf31
redaxmedia <info@redaxmedia.com>
2022-02-19 20:04:26 +0100

    Core based testing for datadog

--- tests/producer/datadog/test_core.py
+++ tests/producer/datadog/test_core.py
@@ -1,26 +1,30 @@
+import argparse
 from typing import Any, get_args
 from unittest.mock import patch
 
-from chroma_feedback.producer.datadog.core import fetch
+from chroma_feedback.producer import datadog
 from chroma_feedback.typing import Status
 
 
 @patch('requests.get')
-def test_fetch_slug(request_mock : Any) -> None:
+def test_run_one(request_mock : Any) -> None:
 	request_mock.return_value.status_code = 200
 	request_mock.return_value.json.return_value =\
 	{
 		'name': 'chroma-feedback',
 		'overall_state': 'OK'
 	}
-	result = fetch('https://api.datadoghq.com', '1', '__api_key__', '__application_key__')
+	datadog.core.ARGS = argparse.Namespace(
+		datadog_host = 'https://api.datadoghq.com',
+		datadog_slug =
+		[
+			'1'
+		],
+		datadog_api_key = '__api_key__',
+		datadog_application_key = '__application_key__'
+	)
+	result = datadog.core.run()
 
 	assert result[0]['name'] == 'datadog'
 	assert result[0]['slug'] == 'chroma-feedback'
 	assert result[0]['status'] in get_args(Status)
-
-
-def test_fetch_invalid() -> None:
-	result = fetch(None, None, None, None)
-
-	assert not result
