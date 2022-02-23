33cc97674d4ca29c965229e118d99a0dabc134e4
redaxmedia <info@redaxmedia.com>
2022-02-23 21:41:58 +0100

    Refactor bamboo to match testing

--- tests/producer/bamboo/test_core.py
+++ tests/producer/bamboo/test_core.py
@@ -1,27 +1,36 @@
+import argparse
 from typing import Any, get_args
 from unittest.mock import patch
 
-from chroma_feedback.producer.bamboo.core import fetch
+from chroma_feedback.producer import bamboo
 from chroma_feedback.typing import Status
 
 
 @patch('requests.get')
-def test_fetch_plan_slug(request_mock : Any) -> None:
+def test_run_one(request_mock : Any) -> None:
 	request_mock.return_value.status_code = 200
 	request_mock.return_value.json.return_value =\
 	{
-		'key': 'redaxmedia-chroma_feedback',
+		'key': 'redaxmedia-chroma-feedback',
 		'buildState': 'Successful'
 	}
-	result = fetch('http://localhost', 'redaxmedia-chroma_feedback', '__token__')
+	bamboo.core.ARGS = argparse.Namespace(
+		bamboo_host = 'http://localhost',
+		bamboo_slug =
+		[
+			'redaxmedia/chroma-feedback'
+		],
+		bamboo_token = '__token__'
+	)
+	result = bamboo.run()
 
 	assert result[0]['name'] == 'bamboo'
-	assert result[0]['slug'] == 'redaxmedia-chroma_feedback'
+	assert result[0]['slug'] == 'redaxmedia-chroma-feedback'
 	assert result[0]['status'] in get_args(Status)
 
 
 @patch('requests.get')
-def test_fetch_project_slug(request_mock : Any) -> None:
+def test_run_many(request_mock : Any) -> None:
 	request_mock.return_value.status_code = 200
 	request_mock.return_value.json.return_value =\
 	{
@@ -30,20 +39,22 @@ def test_fetch_project_slug(request_mock : Any) -> None:
 			'result':
 			[
 				{
-					'key': 'redaxmedia-chroma_feedback',
+					'key': 'redaxmedia-chroma-feedback',
 					'buildState': 'Successful'
 				}
 			]
 		}
 	}
-	result = fetch('http://localhost', 'redaxmedia', '__token__')
+	bamboo.core.ARGS = argparse.Namespace(
+		bamboo_host = 'http://localhost',
+		bamboo_slug =
+		[
+			'redaxmedia'
+		],
+		bamboo_token = '__token__'
+	)
+	result = bamboo.run()
 
 	assert result[0]['name'] == 'bamboo'
-	assert result[0]['slug'] == 'redaxmedia-chroma_feedback'
+	assert result[0]['slug'] == 'redaxmedia-chroma-feedback'
 	assert result[0]['status'] in get_args(Status)
-
-
-def test_fetch_invalid() -> None:
-	result = fetch(None, None, None)
-
-	assert not result
