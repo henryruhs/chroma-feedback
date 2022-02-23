43694f3081e06618c6a8f68e1ed59db639b6ccee
redaxmedia <info@redaxmedia.com>
2022-02-23 21:20:31 +0100

    Refactor azure to match testing

--- tests/producer/azure/test_core.py
+++ tests/producer/azure/test_core.py
@@ -1,12 +1,13 @@
+import argparse
 from typing import Any, get_args
 from unittest.mock import patch
 
-from chroma_feedback.producer.azure.core import fetch
+from chroma_feedback.producer import azure
 from chroma_feedback.typing import Status
 
 
 @patch('requests.get')
-def test_fetch_slug(request_mock : Any) -> None:
+def test_run_one(request_mock : Any) -> None:
 	request_mock.return_value.status_code = 200
 	request_mock.return_value.json.return_value =\
 	{
@@ -22,14 +23,16 @@ def test_fetch_slug(request_mock : Any) -> None:
 			}
 		]
 	}
-	result = fetch('https://dev.azure.com', 'redaxmedia/chroma-feedback', '__token__')
+	azure.core.ARGS = argparse.Namespace(
+		azure_host = 'https://dev.azure.com',
+		azure_slug =
+		[
+			'redaxmedia/chroma-feedback'
+		],
+		azure_token = '__token__'
+	)
+	result = azure.run()
 
-	assert result[0]['name'] == 'azure'
+	assert result[0]['name'] == 'azure' 
 	assert result[0]['slug'] == 'chroma-feedback'
 	assert result[0]['status'] in get_args(Status)
-
-
-def test_fetch_invalid() -> None:
-	result = fetch(None, None, None)
-
-	assert not result
