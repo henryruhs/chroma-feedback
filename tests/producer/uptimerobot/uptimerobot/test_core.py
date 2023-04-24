c0262c34c3d7b62d0712c3dbd4bdccd65f1d7487
henryruhs <info@henryruhs.com>
2023-04-24 11:59:24 +0200

    Fix typo in test

--- tests/producer/uptimerobot/uptimerobot/test_core.py
+++ tests/producer/uptimerobot/uptimerobot/test_core.py
@@ -13,14 +13,14 @@ def test_run_one() -> None:
 			uptimerobot_host = 'https://api.uptimerobot.com',
 			uptimerobot_slug =
 			[
-				'chrome-feedback-test'
+				'chroma-feedback-test'
 			],
 			uptimerobot_token = os.environ.get('UPTIMEROBOT_TOKEN')
 		)
 		result = uptimerobot.core.run()
 
 		assert result[0]['name'] == 'uptimerobot'
-		assert result[0]['slug'] == 'chrome-feedback-test'
+		assert result[0]['slug'] == 'chroma-feedback-test'
 		assert result[0]['status'] in get_args(Status)
 	else:
 		pytest.skip('UPTIMEROBOT_TOKEN is not defined')
