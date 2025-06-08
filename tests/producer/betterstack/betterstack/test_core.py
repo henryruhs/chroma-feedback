f0a2cc7e2a2d601dcf3421a649dd67435f2ca830
henryruhs <info@henryruhs.com>
2025-06-08 09:26:45 +0200

    Add betterstack as producer

--- tests/producer/betterstack/betterstack/test_core.py
+++ tests/producer/betterstack/betterstack/test_core.py
@@ -3,13 +3,13 @@ from typing import get_args
 
 import pytest
 
-from chroma_feedback.producer.uptimerobot import uptimerobot
+from chroma_feedback.producer.betterstack import betterstack
 from chroma_feedback.types import Status
 
 
 def test_run_one() -> None:
 	if os.environ.get('BETTERSTACK_TOKEN'):
-		uptimerobot.core.ARGS =\
+		betterstack.core.ARGS =\
 		{
 			'betterstack_host': 'https://uptime.betterstack.com',
 			'betterstack_slug':
@@ -18,7 +18,7 @@ def test_run_one() -> None:
 			],
 			'betterstack_token': os.environ.get('BETTERSTACK_TOKEN')
 		}
-		result = uptimerobot.core.run()
+		result = betterstack.core.run()
 
 		assert result[0]['name'] == 'uptimerobot'
 		assert result[0]['slug'] == 'chroma-feedback-test'
@@ -29,13 +29,13 @@ def test_run_one() -> None:
 
 def test_run_many() -> None:
 	if os.environ.get('BETTERSTACK_TOKEN'):
-		uptimerobot.core.ARGS =\
+		betterstack.core.ARGS =\
 		{
 			'betterstack_host': 'https://uptime.betterstack.com',
 			'betterstack_slug': None,
 			'betterstack_token': os.environ.get('BETTERSTACK_TOKEN')
 		}
-		result = uptimerobot.core.run()
+		result = betterstack.core.run()
 
 		assert result[0]['name'] == 'uptimerobot'
 		assert result[0]['slug']
